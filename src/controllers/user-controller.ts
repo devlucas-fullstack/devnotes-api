1d9de44935a07811086835613f454b492c4774c1
Lucas Vinicius <lucasv2224@outlook.com>
2026-01-15 20:47:09 -0300

    feat: adds try catch for exception treatment

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,38 +1,44 @@
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 import prisma from "@/database/prisma";
 import { z } from "zod";
 import { hash } from "bcrypt";
 import { AppError } from "@/utils/AppError";
 
 class UserController {
-  async create(req: Request, res: Response) {
-    const bodySchema = z.object({
-      name: z.string().min(2, { message: "Informe seu nome" }),
-      email: z.string().email({ message: "Informe um e-mail válido" }),
-      password: z
-        .string()
-        .min(6, { message: "Senha deve ter no mínimo 6 dígitos" }),
-    });
+  async create(req: Request, res: Response, next: NextFunction) {
+    try {
+      const bodySchema = z.object({
+        name: z.string().min(2, { message: "Informe seu nome" }),
+        email: z.string().email({ message: "Informe um e-mail válido" }),
+        password: z
+          .string()
+          .min(6, { message: "Senha deve ter no mínimo 6 dígitos" }),
+      });
 
-    const { email, name, password } = bodySchema.parse(req.body);
+      const { email, name, password } = bodySchema.parse(req.body);
 
-    const hashedPassword = await hash(password, 8);
+      const hashedPassword = await hash(password, 8);
 
-    const userWithSameEmail = await prisma.user.findFirst({ where: { email } });
+      const userWithSameEmail = await prisma.user.findFirst({
+        where: { email },
+      });
 
-    if (userWithSameEmail) {
-      throw new AppError("Endereço de e-mail já existe!");
+      if (userWithSameEmail) {
+        throw new AppError("Endereço de e-mail já existe!");
+      }
+
+      await prisma.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      res.status(201).json();
+    } catch (error) {
+      next(error);
     }
-
-    await prisma.user.create({
-      data: {
-        name,
-        email,
-        password: hashedPassword,
-      },
-    });
-
-    res.status(201).json();
   }
 }
 
